b102fb3b4629ce9b6b7978999573ca7440a3b9c3
Banu <banurajamani@gmail.com>
2018-10-04 16:10:46 +0530

    JSON Object loaded, DetailActivity View constructed for all Sandwitch elements

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,31 +3,55 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.stream.Collectors;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    //View Holders
+    private Sandwich sandwich = null;
+    private ImageView ingredientsIv = null;
+    private TextView originTv = null;
+    private TextView aliasTv = null;
+    private TextView descTv = null;
+    private TextView ingTv  = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        //Initialize the View Holders
+        ingredientsIv = findViewById(R.id.image_iv);
+        originTv      = findViewById(R.id.origin_tv);
+        aliasTv       = findViewById(R.id.also_known_tv);
+        descTv        = findViewById(R.id.description_tv);
+        ingTv         = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        int position = DEFAULT_POSITION;
+
+        try {
+             position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        }
+        catch (NullPointerException e){
+            closeOnError();
+        }
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -36,7 +60,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -44,9 +68,6 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -57,6 +78,33 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        //Hide the image completely and show it if images gets loaded.
+        ingredientsIv.setVisibility(View.GONE);
+        // Load image and any error hide the image view
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(ingredientsIv, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        ingredientsIv.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onError() {
+                        ingredientsIv.setVisibility(View.GONE);
+                    }
+                });
+
+
+        originTv.setText(sandwich.getPlaceOfOrigin());
+        //List of Also known as Names concatenated using commas
+        aliasTv.setText(TextUtils.join(",", sandwich.getAlsoKnownAs()));
+        descTv.setText(sandwich.getDescription() );
+        //List of Ingredients concatenated using commas
+        ingTv.setText(TextUtils.join(",", sandwich.getIngredients()));
+
+
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,54 @@
 package com.udacity.sandwichclub.utils;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+
+        try {
+
+            JSONObject sandwichJSONObj = new JSONObject(json);
+            JSONObject nameJSONObj = sandwichJSONObj.getJSONObject("name");
+            JSONArray aliasJSONArray = nameJSONObj.getJSONArray("alsoKnownAs");
+            ArrayList<String> aliasList = new ArrayList<>();
+            if (aliasJSONArray != null) {
+                for (int i = 0; i < aliasJSONArray.length(); i++) {
+                    aliasList.add(aliasJSONArray.getString(i));
+                }
+            }
+            JSONArray ingredientsJSONArray = sandwichJSONObj.getJSONArray("ingredients");
+            ArrayList<String> ingredientsList = new ArrayList<>();
+            for (int i=0; i< ingredientsJSONArray.length(); i++){
+                ingredientsList.add(ingredientsJSONArray.getString(i));
+            }
+            Sandwich sandwitchObj = new Sandwich(
+                    nameJSONObj.getString("mainName"),
+                    aliasList,
+                    sandwichJSONObj.getString("placeOfOrigin"),
+                    sandwichJSONObj.getString("description"),
+                    sandwichJSONObj.getString("image"),ingredientsList);
+
+            return sandwitchObj;
+
+
+
+        }
+        catch (JSONException e){
+            Log.e("JSONUtils", e.getMessage());
+
+        }
+
         return null;
     }
 }
